f321235f23d333f09876e94c97000d641d30a20a
wwyzhao <1733752642@qq.com>
2023-03-04 01:14:33 -0500

    redo problem3 with copulas

--- Week05/problem3.py
+++ Week05/problem3.py
@@ -4,68 +4,63 @@ sys.path.append(BASE_DIR)
 
 import numpy as np
 import pandas as pd
-from RiskMgmt import VaR, ES
+import scipy.stats as st
+from RiskMgmt import VaR, ES, Simulation
+import warnings
+warnings.filterwarnings("ignore") 
 
+def copula_t(rt, nsim = 1000):
+    n = rt.shape[1]
+    rt = (rt - rt.mean()).astype(np.float64)
 
-# using the two csv files to get the daily total value of each portfolio, preparing for calculating returns
-def parse_data(p, df):
-    tot_value = pd.DataFrame(index=df.index, columns=["Total Value"])
-    m = df.shape[0]
+    # calculate each T parameters, CDF for each column
+    paras = [st.t.fit(rt[col]) for col in rt.columns]
+    cdf = st.t.cdf(rt, *zip(*paras))
+    cdf = pd.DataFrame(data=cdf, index=rt.index, columns=rt.columns)
+    
+    # convert from multivariate normal to simulations of a T distribution
+    corr = cdf.corr(method = "spearman")
+    sim_t = pd.DataFrame(st.norm.cdf(Simulation.simulate_pca(corr, nsim, 1)))
+    sim_df = [st.t.ppf(sim_t[col], *zip(*paras)) for col in sim_t.columns]
+    sim_df = pd.DataFrame(data=sim_df, columns=rt.columns)
 
-    # for data on each date
-    for i in range(m):
-        current = pd.DataFrame({"Stock":df.columns, "price":df.iloc[i,:]}) 
-        #get the prices and holdings of each stock, calculate daily total price of all the holdings in a portfolio
-        temp = pd.merge(current, p, on="Stock", how="inner")    
-        temp_sum = sum(temp["price"] * temp["Holding"])
-        tot_value.iloc[i][0] = temp_sum
-    return tot_value
-
+    return sim_df
+    
 
 if __name__ == '__main__':
 
     df = pd.read_csv("DailyPrices.csv", index_col="Date")
-    port = pd.read_csv("portfolio.csv").groupby("Portfolio")
-
-    # total value of 3 portfolios on each day
-    tot_port_value = pd.DataFrame(index=df.index, columns=["Total Port Value"])
-    tot_port_value["Total Port Value"] = 0
-    price_latest_value = [] # latest price of each portfolio
-
-    # Arithmetric Return and T fitted VaR and ES
-    # for each portfolio, get the daily total value, calculate Arithmetric Return
-    rt_port_list = [] # return list of 3 portfolios
-    for p in port:
-        tot_value = parse_data(p[1], df)
-        tot_port_value["Total Port Value"] += tot_value["Total Value"] # get the total value of 3 portfolios on each day
-        price_latest_value.append(tot_value.iloc[-1]["Total Value"])
-        
-        # return for each of the 3 protfolios, mean = 0
-        rt_ARITH = VaR.return_calculate(tot_value, "ARITH_RT")
-        rt_ARITH = rt_ARITH.loc[:, "Total Value"]
-        rt_ARITH = np.array(rt_ARITH[1:]).astype(np.float64)
-        rt_ARITH = rt_ARITH - np.mean(rt_ARITH)
-        rt_port_list.append(rt_ARITH)
-
-    # return for total 3 portfolios, mean = 0
-    rt_tot_ARITH = VaR.return_calculate(tot_port_value, "ARITH_RT")
-    rt_tot_ARITH = rt_tot_ARITH.loc[:, "Total Port Value"]
-    rt_tot_ARITH = np.array(rt_tot_ARITH[1:]).astype(np.float64)
-    rt_tot_ARITH = rt_tot_ARITH - np.mean(rt_tot_ARITH)
-
-    alpha = 0.05
-
-    VaR_T, ES_T = ES.get_VaR_ES_T(rt_port_list[0], alpha)
-    print("Portfolio A:")  
-    print(f"VaR using a T distribution: ${price_latest_value[0] * VaR_T}, ES: ${price_latest_value[0] * ES_T}")
-    VaR_T, ES_T = ES.get_VaR_ES_T(rt_port_list[1], alpha)
-    print("Portfolio B:")  
-    print(f"VaR using a T distribution: ${price_latest_value[1] * VaR_T}, ES: ${price_latest_value[1] * ES_T}")
-    VaR_T, ES_T = ES.get_VaR_ES_T(rt_port_list[2], alpha)
-    print("Portfolio C:")  
-    print(f"VaR using a T distribution: ${price_latest_value[2] * VaR_T}, ES: ${price_latest_value[2] * ES_T}")
-    VaR_T, ES_T = ES.get_VaR_ES_T(rt_tot_ARITH, alpha)
-    print("VaR of total holdings")  
-    print(f"VaR using a T distribution: ${sum(price_latest_value) * VaR_T}, ES: ${sum(price_latest_value) * ES_T}")
+    # current price of each stock
+    cur_price = df.iloc[df.shape[0]-1,:]
+    cur_price.index.name = "Stock"
+    cur_price = pd.DataFrame({"price": cur_price}, index=cur_price.index)
     
-
+    rt = VaR.return_calculate(df, "ARITH_RT")
+    rt = rt[1:]
+    nsim = 1000
+    sim_df = copula_t(rt, nsim)
+    
+    # get simulated total value on the following 1000 days of each portfolio and all 3 portfolios
+    ports = pd.read_csv("portfolio.csv").groupby("Portfolio")
+    sum_port = pd.DataFrame()
+    for name, port in ports:
+        df_port = pd.merge(port, cur_price, on="Stock", how="inner")
+        cur_value = sum(df_port["Holding"] * df_port["price"]) # current value
+        
+        sim_value = []
+        for i in range(nsim):
+            r = sim_df.iloc[i,:]
+            r.index.name = "Stock"
+            r = pd.DataFrame({"return": r}, index=r.index)
+            df_temp = pd.merge(df_port, r, on="Stock", how="inner")
+            fu_value = sum(df_temp["Holding"] * df_temp["price"] * (1 + df_temp["return"])) # future value using simulated return
+            dif = fu_value - cur_value
+            sim_value.append(dif)
+        
+        sum_port[name] = sim_value
+    sum_port["total"] = sum_port["A"] + sum_port["B"] + sum_port["C"]
+    
+    for name, port in sum_port.iteritems():
+        VaR_his, ES_his = ES.get_VaR_ES_historic(port, alpha=0.05)
+        print("Portfolio", name, "VaR: ", VaR_his, "ES: ", ES_his)
+            
